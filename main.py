2e5dc6dcda7cb60eddca625d8d397fecc4f89cf7
egori4 <egori4@gmail.com>
2022-11-04 16:00:45 -0500

    First

---
+++ main.py
@@ -0,0 +1,37 @@
+
+#read from file
+with open('attackipsid_input.txt', 'r') as f:
+	attackipsid = f.read().splitlines()
+
+#remove duplicates
+attackipsid = list(dict.fromkeys(attackipsid))
+#print list
+print(attackipsid)
+
+
+for id in attackipsid:
+	#18260860-1663963190
+	id_first_part_dec = id.split('-')[0]
+	id_second_part_dec = id.split('-')[1]
+	#convert id_first_part_dec to hex removing 0x
+	id_first_part_hex = hex(int(id_first_part_dec))[2:]
+	
+	# create new var id_first_part_hex_prefix which includes only first 3 chars of id_first_part_hex
+	id_first_part_hex_prefix = id_first_part_hex[:3]
+	
+	#remove first 3 chars from id_first_part_hex
+	id_first_part_hex = id_first_part_hex[3:]
+
+	#convert id_second_part_dec to hex removing 0x
+	id_second_part_hex = hex(int(id_second_part_dec))[2:]
+
+	id_second_part_hex = '0' + id_first_part_hex_prefix + id_second_part_hex
+
+	#create new string = FFFFFFFF-FFFF-FFFF- + id_first_part_hex + id_second_part_hex
+	syslog_id = 'FFFFFFFF-FFFF-FFFF-' + id_first_part_hex + '-' + id_second_part_hex
+
+	print(f'AttackIpsID {id} , syslog ID {syslog_id}')
+
+
+
+

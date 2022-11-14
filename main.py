79937717bd2ce20adc353293ccea6c71837d1b14
egori4 <egori4@gmail.com>
2022-11-14 09:14:34 -0600

    bugfix

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ for id in attackipsid:
 
 		if len(id_first_part_dec) == 8 and len(id_second_part_dec) == 10:
 			#If the first part of the ID is 8 digits, the second part is 10 digits
-			# print('id_first_part_dec is 8, id_second_part_dec is 10')
 			
 			# create new var id_first_part_hex_prefix which includes only first 3 chars of id_first_part_hex
 			id_first_part_hex_prefix = id_first_part_hex[:3]
@@ -26,7 +25,6 @@ for id in attackipsid:
 			#remove first 3 chars from id_first_part_hex
 			id_first_part_hex = id_first_part_hex[3:]
 
-			#convert id_second_part_dec to hex removing 0x
 			id_second_part_hex = '0' + id_first_part_hex_prefix + id_second_part_hex
 
 			#create new string = FFFFFFFF-FFFF-FFFF- + id_first_part_hex + id_second_part_hex
@@ -59,17 +57,17 @@ for id in attackipsid:
 			id_first_part_hex = id_first_part_hex[1:]
 
 			#convert id_second_part_dec to hex removing 0x
-			id_second_part_hex = '0000' + id_first_part_hex_prefix + id_second_part_hex
+			id_second_part_hex = '000' + id_first_part_hex_prefix + '0' + id_second_part_hex
 
 			#create new string = FFFFFFFF-FFFF-FFFF- + id_first_part_hex + id_second_part_hex
 			syslog_id = 'FFFFFFFF-FFFF-FFFF-' + id_first_part_hex + '-' + id_second_part_hex
 
-			print(f'AttackIpsID {id} , syslog ID {syslog_id}, This conversion needs to be tested further and validated!!!')
+			print(f'AttackIpsID {id} , syslog ID {syslog_id}')
 		
 		
 		
 		else:
-			print(f'ID: {id} does not match any condition for conversion')
+			print(f'AttackIpsID {id} does not match any condition for conversion')
 
 
 
